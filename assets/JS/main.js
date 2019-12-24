1a31cb40d15403606d880f0c6e235480dce0b8f6
brer4449 <58540640+brer4449@users.noreply.github.com>
2019-12-23 21:44:37 -0500

    created variables

---
+++ assets/JS/main.js
@@ -0,0 +1,17 @@
+var specialChar = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~", "\'", "\"", "\\" ]
+    console.log(specialChar[30]);
+    console.log(specialChar[31]);
+    console.log(specialChar[32]);
+
+var numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+console.log(numChar[9]);
+
+var lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+console.log(lowChar[25]);
+
+var upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+console.log(upChar[25]);
+
+// var contains = function(x){
+//     for(var i = 0; i<)
+// }
