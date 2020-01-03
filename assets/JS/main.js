506e1f71ee16c46c0aa3c43529f314d240b3524f
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-03 14:24:03 -0500

    swapped some sstuff around

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -24,12 +24,14 @@ let checkup = document.getElementById("check-up");
 let checknum = document.getElementById("check-num");
 let checkspec = document.getElementById("check-special");
 
-checklow.value = lowChar;
-checkup.value = upChar;
-checknum.value = numChar;
-checkspec.value = specialChar;
+// lowChar = checklow.value;
+// upChar = checkup.value;
+// numChar = checknum.value;
+// checkspec = specialChar.value;
 
-let passwordText = document.querySelector("#password");
+// let passwordContent = document.getElementById("password").textContent
+// console.log(passwordContent);
+let passwordText = document.querySelector("#password").textContent;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -38,39 +40,40 @@ generateBtn.addEventListener("click", randomPass)
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+// function writePassword() {
+//   var password = generatePassword();
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
-}
+//   copyBtn.removeAttribute("disabled");
+//   copyBtn.focus();
+// }
 
 function randomPass(){
     // loop from 1 to number of characters in password
     // for each one, choose one of the types allowed
     // pick a value from that type and add it to the password string
-
-    let randomUp = upChar[getRandomInt(0, 25)];
-    let randomLow = lowChar[getRandomInt(0, 25)];
-    let randomNum = numChar[getRandomInt(0, 9)];
-    let randomSpec = specialChar[getRandomInt(0, 32)];
+    
+    // let randomUp = upChar[getRandomInt(0, 25)];
+    // let randomLow = lowChar[getRandomInt(0, 25)];
+    // let randomNum = numChar[getRandomInt(0, 9)];
+    // let randomSpec = specialChar[getRandomInt(0, 32)];
 
     let randoInt = getRandomInt(2, 32)
+
     for (let i = 0; i < randoInt; i++){
         randomUp = upChar[getRandomInt(0, 25)];
-        console.log(randomUp)
+        // console.log(randomUp)
         let randomLow = lowChar[getRandomInt(0, 25)];
-        console.log(randomLow);
+        // console.log(randomLow);
         let randomNum = numChar[getRandomInt(0, 9)];
-        console.log(randomNum);
+        // console.log(randomNum);
         let randomSpec = specialChar[getRandomInt(0, 32)];
-        console.log(randomSpec);
+        // console.log(randomSpec);
         console.log(randoInt);
         // if (i != randoInt - 1 ) { continue; }
         // else {
-            console.log(passwordText+= randomUp + randomLow + randomNum + randomSpec);
+            passwordText+= randomUp + randomLow + randomNum + randomSpec;
         // }
         console.log(passwordText);
         
@@ -83,9 +86,9 @@ function randomPass(){
 };
 randomPass();
 
-function copyToClipboard() {
+// function copyToClipboard() {
   // BONUS 
-}
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
