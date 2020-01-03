70d92945541c04e3c87b1d9342593f1b45a23035
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-02 22:35:38 -0500

    realized i was doing it wrong...

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,17 +1,88 @@
-var specialChar = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~", "\'", "\"", "\\" ]
-    console.log(specialChar[30]);
-    console.log(specialChar[31]);
-    console.log(specialChar[32]);
+const specialChar = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~", "\'", "\"", "\\" ]
+// console.log(specialChar[32]);
+// console.log(specialChar[getRandomInt(0, 32)]);
+const numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+// console.log(numChar[9]);
+// console.log(numChar[getRandomInt(0, 9)]);
+const lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// console.log(lowChar[25]);
+// console.log(lowChar[getRandomInt(0, 25)]);
+const upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// console.log(upChar[25]);
+// console.log(upChar[getRandomInt(0, 25)]);
 
-var numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-console.log(numChar[9]);
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; 
+}
+console.log(getRandomInt(8, 128));
 
-var lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-console.log(lowChar[25]);
+let checklow = document.getElementById("check-low");
+let checkup = document.getElementById("check-up");
+let checknum = document.getElementById("check-num");
+let checkspec = document.getElementById("check-special");
 
-var upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-console.log(upChar[25]);
+checklow.value = lowChar;
+checkup.value = upChar;
+checknum.value = numChar;
+checkspec.value = specialChar;
 
-// var contains = function(x){
-//     for(var i = 0; i<)
-// }
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  copyBtn.removeAttribute("disabled");
+  copyBtn.focus();
+}
+
+function randomPass(){
+    // loop from 1 to number of characters in password
+    // for each one, choose one of the types allowed
+    // pick a value from that type and add it to the password string
+
+    let randomUp = upChar[getRandomInt(0, 25)];
+    let randomLow = lowChar[getRandomInt(0, 25)];
+    let randomNum = numChar[getRandomInt(0, 9)];
+    let randomSpec = specialChar[getRandomInt(0, 32)];
+
+    let randoInt = getRandomInt(2, 32)
+    for (let i = 0; i < randoInt; i++){
+        randomUp = upChar[getRandomInt(0, 25)];
+        console.log(randomUp)
+        let randomLow = lowChar[getRandomInt(0, 25)];
+        console.log(randomLow);
+        let randomNum = numChar[getRandomInt(0, 9)];
+        console.log(randomNum);
+        let randomSpec = specialChar[getRandomInt(0, 32)];
+        console.log(randomSpec);
+        console.log(randoInt);
+        // if (i != randoInt - 1 ) { continue; }
+        // else {
+            console.log(genPass+= randomUp + randomLow + randomNum + randomSpec);
+        // }
+        
+    } 
+    console.log(genPass);
+    // event.preventDefault();
+    // console.log(event.target)
+    return genPass;
+        
+};
+randomPass();
+
+function copyToClipboard() {
+  // BONUS 
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// BONUS EVENT LISTENER
