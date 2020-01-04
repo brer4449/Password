c26e423401519fcdbc899394a713721a5c4436de
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-04 12:32:45 -0500

    added function to test if checkbox checked (wip)

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,16 +1,14 @@
+//eventhandler for form submission
 
-const specialChar = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~", "\'", "\"", "\\" ]
-// console.log(specialChar[32]);
-// console.log(specialChar[getRandomInt(0, 32)]);
-const numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-// console.log(numChar[9]);
-// console.log(numChar[getRandomInt(0, 9)]);
-const lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-// console.log(lowChar[25]);
-// console.log(lowChar[getRandomInt(0, 25)]);
-const upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-// console.log(upChar[25]);
-// console.log(upChar[getRandomInt(0, 25)]);
+//determine what they entered into the form(number of characters and which other options)
+    //make sure password length is a number 
+    //make sure at least one of the options is selected
+//make a new variable the contains all the characters allowed in the password ✓
+//loop through how many characters the password should be
+    //for each time through the loop
+    //get a random character from the desired characters
+    //then add that to the new password string
+//when done, display new password
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -19,19 +17,68 @@ function getRandomInt(min, max) {
 }
 console.log(getRandomInt(8, 128));
 
+const lowChar = "abcdefghijklmnopqrstuvwxyz"
+const upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// console.log(upChar[25]);
+const numChar = "1234567890"
+const specialChar = "!#$%&()*+-/:;<=>?@[]^_{}|~"
+// console.log(specialChar[25]);
+
+let passwordContent;
+
+passwordContent = [lowChar, upChar, numChar, specialChar];
+console.log(passwordContent);
+
+let passwordCombination;
+
+
+let passwordLength = document.getElementById("#passwordLength");
+let passwordText = document.querySelector("#password").textContent;
+
+
+//attempt at making sure something got entered, and that it was a number
+// if (passwordLength === null) {
+//     alert("Try again!");
+// } else if (passwordText.toString() !== numChar){
+//     alert("Try again!");
+// } else {
+//     passwordText = passwordLength;
+// };
+
+
 let checklow = document.getElementById("check-low");
 let checkup = document.getElementById("check-up");
 let checknum = document.getElementById("check-num");
 let checkspec = document.getElementById("check-special");
 
-// lowChar = checklow.value;
-// upChar = checkup.value;
-// numChar = checknum.value;
-// checkspec = specialChar.value;
+function getCheckedState(event) {
+    event.preventDefault();
+    let input = document.querySelectorAll("form-check-input");
+    let isChecked = input.checked;
+    if(isChecked !== false){
+        alert("The checkbox is clicked");
+    } else {
+        alert("The checkbox is not clicked");
+    };
+}
 
-// let passwordContent = document.getElementById("password").textContent
-// console.log(passwordContent);
-let passwordText = document.querySelector("#password").textContent;
+checklow.addEventListener("click", getCheckedState)
+
+// if (document.getElementsByClassName("form-check-input").checked == true){
+//     if (checklow.checked == true){
+//         passwordCombination += passwordContent[0];
+//     } else if (checkup.checked == true){
+//         passwordCombination += passwordContent[1];
+//     } else if (checknum.checked == true){
+//         passwordCombination += passwordContent[2];
+//     } else{
+//         passwordCombination += passwordContent[3];
+//     }; console.log(passwordCombination);
+// } else {
+//     alert ("At least one box must be checked!")
+// };
+
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -53,11 +100,6 @@ function randomPass(){
     // loop from 1 to number of characters in password
     // for each one, choose one of the types allowed
     // pick a value from that type and add it to the password string
-    
-    // let randomUp = upChar[getRandomInt(0, 25)];
-    // let randomLow = lowChar[getRandomInt(0, 25)];
-    // let randomNum = numChar[getRandomInt(0, 9)];
-    // let randomSpec = specialChar[getRandomInt(0, 32)];
 
     let randoInt = getRandomInt(2, 32)
 
@@ -70,15 +112,15 @@ function randomPass(){
         // console.log(randomNum);
         let randomSpec = specialChar[getRandomInt(0, 32)];
         // console.log(randomSpec);
-        console.log(randoInt);
+        // console.log(randoInt);
         // if (i != randoInt - 1 ) { continue; }
         // else {
             passwordText+= randomUp + randomLow + randomNum + randomSpec;
         // }
-        console.log(passwordText);
+        // console.log(passwordText);
         
     } 
-    console.log(passwordText);
+    // console.log(passwordText);
     // event.preventDefault();
     // console.log(event.target)
     return passwordText;
@@ -91,6 +133,6 @@ randomPass();
 // }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
