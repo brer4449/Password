e7b97e726af5c273bc092a66859922a82e1e587c
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-28 17:50:11 -0500

    refactored! added 3 sec error message for wrong number input

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,90 +1,108 @@
-const lowChar = "abcdefghijklmnopqrstuvwxyz"
-const upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numChar = "1234567890"
-const specialChar = "!#$%&()*+-/:;<=>?@[]^_{}|~"
+const lowChar = "abcdefghijklmnopqrstuvwxyz";
+const upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numChar = "1234567890";
+const specialChar = "!#$%&()*+-/:;<=>?@[]^_{}|~";
 
 //Array of different character types
-let passwordContent = [lowChar, upChar, numChar, specialChar];
-//Variable set as blank, so passwordContent can get added to it
+let passwordArray = [lowChar, upChar, numChar, specialChar];
+//Variable set as blank, so passwordArray can get added to it
 let passwordCombination = "";
 //Input value received from user, number entered (returns as string)
 
 //Variable reaching the text area
-let passwordText = document.getElementById("password");
+let passwordEl = document.getElementById("password");
 //Referencing each checkbox by Id
 let checklow = document.getElementById("check-low");
 let checkup = document.getElementById("check-up");
 let checknum = document.getElementById("check-num");
 let checkspec = document.getElementById("check-special");
 //Button variable
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 //Event listener calling the randomPass function
 generateBtn.addEventListener("click", randomPass);
 
 function getCheckedStateLow() {
-    let input = document.getElementById("check-low");
-    let isChecked = input.checked;
-    if (isChecked === true) {
-        passwordCombination += passwordContent[0];
-    } else {
-        if (passwordCombination.includes(passwordContent[0])) {
-            passwordCombination = passwordCombination.replace(passwordContent[0], "");
-
-        }
+  let input = document.getElementById("check-low");
+  let isChecked = input.checked;
+  if (isChecked) {
+    passwordCombination += passwordArray[0];
+  } else {
+    if (passwordCombination.includes(passwordArray[0])) {
+      passwordCombination = passwordCombination.replace(passwordArray[0], "");
     }
-};
+  }
+}
 function getCheckedStateUp() {
-    let input = document.getElementById("check-up");
-    let isChecked = input.checked;
-    if (isChecked === true) {
-        passwordCombination += passwordContent[1];
-    } else {
-        if (passwordCombination.includes(passwordContent[1])) {
-            passwordCombination = passwordCombination.replace(passwordContent[1], "");
-        };
+  let input = document.getElementById("check-up");
+  let isChecked = input.checked;
+  if (isChecked) {
+    passwordCombination += passwordArray[1];
+  } else {
+    if (passwordCombination.includes(passwordArray[1])) {
+      passwordCombination = passwordCombination.replace(passwordArray[1], "");
     }
-};
+  }
+}
 function getCheckedStateNum() {
-    let input = document.getElementById("check-num");
-    let isChecked = input.checked;
-    if (isChecked === true) {
-        passwordCombination += passwordContent[2];
-    } else {
-        if (passwordCombination.includes(passwordContent[2])) {
-            passwordCombination = passwordCombination.replace(passwordContent[2], "");
-
-        }
+  let input = document.getElementById("check-num");
+  let isChecked = input.checked;
+  if (isChecked) {
+    passwordCombination += passwordArray[2];
+  } else {
+    if (passwordCombination.includes(passwordArray[2])) {
+      passwordCombination = passwordCombination.replace(passwordArray[2], "");
     }
-};
+  }
+}
 function getCheckedStateSpecial() {
-    let input = document.getElementById("check-special");
-    let isChecked = input.checked;
-    if (isChecked === true) {
-        passwordCombination += passwordContent[3];
-    } else {
-        if (passwordCombination.includes(passwordContent[3])) {
-            passwordCombination = passwordCombination.replace(passwordContent[3], "");
-
-        }
+  let input = document.getElementById("check-special");
+  let isChecked = input.checked;
+  if (isChecked) {
+    passwordCombination += passwordArray[3];
+  } else {
+    if (passwordCombination.includes(passwordArray[3])) {
+      passwordCombination = passwordCombination.replace(passwordArray[3], "");
     }
-    console.log(passwordCombination);
-};
+  }
+  //   console.log(passwordCombination);
+}
 
 checklow.addEventListener("click", getCheckedStateLow);
 checkup.addEventListener("click", getCheckedStateUp);
 checknum.addEventListener("click", getCheckedStateNum);
 checkspec.addEventListener("click", getCheckedStateSpecial);
 
+function showError(error) {
+  const errorDiv = document.createElement("div");
+  const wrapper = document.querySelector(".wrapper");
+  const header = document.querySelector("#heading");
+  errorDiv.className = "alert alert-danger";
+  errorDiv.appendChild(document.createTextNode(error));
+  wrapper.insertBefore(errorDiv, header);
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector(".alert").remove();
+}
+
 function randomPass(event) {
-    event.preventDefault();
-    passwordText.textContent = "";
-    let passwordLength = parseInt(document.getElementById("passwordLength").value);
-
+  event.preventDefault();
+  passwordEl.textContent = "";
+  let passwordLength = parseInt(
+    document.getElementById("passwordLength").value
+  );
+  if (passwordLength >= 8 && passwordLength <= 128) {
     for (let i = 0; i < passwordLength; i++) {
-
-        let randomNum = Math.floor(Math.random() * (passwordCombination.length - 1) + 1);
-        passwordText.textContent += passwordCombination[randomNum];
-        console.log(passwordCombination);
+      let randomNum = Math.floor(
+        Math.random() * (passwordCombination.length - 1) + 1
+      );
+      passwordEl.textContent += passwordCombination[randomNum];
+      console.log(passwordCombination);
     }
-    return passwordText;
-};
+    return passwordEl;
+  } else {
+    showError("Please check your numbers");
+    return;
+  }
+}
